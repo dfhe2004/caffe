98319af23c8a9c5b69a77619b4ea42fed93f5a7e
david <send@to.me>
2016-12-01 14:02:11 -0700

    .

--- src/ex/util/interp.cu
+++ src/ex/util/interp.cu
@@ -1,8 +1,24 @@
 // Copyright 2014 George Papandreou
 
 #include "caffe/common.hpp"
-#include "ex/common.cuh"
 #include "ex/interp.hpp"
+
+/*
+#if __CUDA_ARCH__ <= 520
+	static __inline__ __device__ double atomicAdd(double *address, double val) {
+	  unsigned long long int* address_as_ull = (unsigned long long int*)address;
+	  unsigned long long int old = *address_as_ull, assumed;
+	  if (val==0.0)
+		return __longlong_as_double(old);
+	  do {
+		assumed = old;
+		old = atomicCAS(address_as_ull, assumed, __double_as_longlong(val +__longlong_as_double(assumed)));
+	  } while (assumed != old);
+	  return __longlong_as_double(old);
+	}
+#endif
+*/
+
 
 namespace caffe {
 
